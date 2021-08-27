3e14c933765c6a5eab2dcb9c2cfa4aafb0f64450
Nate Marks <npmarks@gmail.com>
2021-08-27 09:44:20 -0400

    make i_test seems to be working

--- internal/iam.go
+++ internal/iam.go
@@ -2,11 +2,14 @@ package internal
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/rs/zerolog"
 )
 
+// GetAWSIdentity Get the STS identity for the current session
 func GetAWSIdentity(logger *zerolog.Logger) (Arn, UserId, Account string) {
 
 	// get the aws sdk client config
@@ -24,4 +27,44 @@ func GetAWSIdentity(logger *zerolog.Logger) (Arn, UserId, Account string) {
 		logger.Fatal().Err(err)
 	}
 	return *p.Arn, *p.UserId, *p.Account
+}
+
+func GetAssumeRoleCreds(assumeRoleARN string, logger *zerolog.Logger) (*stscreds.AssumeRoleProvider, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		logger.Fatal().Err(err)
+		return nil, err
+	}
+	stsSvc := sts.NewFromConfig(cfg)
+	if err != nil {
+		logger.Fatal().Err(err)
+		return nil, err
+	}
+	creds := stscreds.NewAssumeRoleProvider(stsSvc, assumeRoleARN)
+	return creds, err
+}
+
+// GetAWSIdentityWithAssumeRole Get the STS identity for the current session
+func GetAWSIdentityWithAssumeRole(assumeRole string, logger *zerolog.Logger) (string, error) {
+
+	// get the aws sdk client config
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		logger.Fatal().Err(err)
+		return "", err
+	}
+	creds, err := GetAssumeRoleCreds(assumeRole, logger)
+	cfg.Credentials = aws.NewCredentialsCache(creds)
+
+	// Create service client value configured for credentials
+	// from assumed role.
+	svc := sts.NewFromConfig(cfg)
+
+	input := &sts.GetCallerIdentityInput{}
+
+	p, err := svc.GetCallerIdentity(context.TODO(), input)
+	if err != nil {
+		logger.Fatal().Err(err)
+	}
+	return *p.Arn, err
 }
--- internal/iam_test.go
+++ internal/iam_test.go
@@ -26,8 +26,40 @@ func ARNMatchesUsername(iARN, iName string) error {
 		msg := fmt.Sprintf("ARN doesn't match username: %s", iName)
 		return errors.New(msg)
 	}
+	if !strings.HasPrefix(a.Resource, "user") {
+		return errors.New("ARN doesn't seem to refer to a user object")
+	}
 	return nil
 }
+
+// ARNMatchesAssumedRole Check the expected assume role name
+// Make sure the arn looks right based on the role name. the are names are prefixed, so
+// partially random
+// example:
+// arn:aws:sts::709310380790:assumed-role/easyaws_iam_assume_role/aws-go-sdk-1630070768490628000
+func ARNMatchesAssumedRole(arARN, assumedRoleName string) error {
+	a, err := arn.Parse(arARN)
+	if err != nil {
+		return err
+	}
+	if a.Service != "sts" {
+		return errors.New("ARN is not for the STS service")
+	}
+	if !strings.Contains(a.Resource, assumedRoleName) {
+		msg := fmt.Sprintf("ARN doesn't contain assumed role name: %s", assumedRoleName)
+		return errors.New(msg)
+	}
+	if !strings.HasPrefix(a.Resource, "assumed-role") {
+		return errors.New("ARN doesn't seem to refer to an assumed role object")
+	}
+	return nil
+}
+
+// TestGetAWSIdentity Run STS get-caller-identity and compare it to the user
+// account created by the project fixture setup
+// (scripts/setup_project_fixtures.sh) The script that runs go test against this
+// must change to the expected credentials before running go test in order for
+// this test to succeed
 func TestGetAWSIdentity(t *testing.T) {
 	myLogger := log.With().Str("test_key", "test_value").Logger()
 	type args struct {
@@ -51,6 +83,66 @@ func TestGetAWSIdentity(t *testing.T) {
 			err := ARNMatchesUsername(gotArn, "test_easyaws")
 			if err != nil {
 				t.Error(err)
+			}
+		})
+	}
+}
+
+func TestGetAssumeRoleCreds(t *testing.T) {
+	myLogger := log.With().Str("test_key", "test_value").Logger()
+	type args struct {
+		assumeRoleARN string
+		logger        *zerolog.Logger
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "valid", args: args{
+			assumeRoleARN: "arn:aws:iam::709310380790:role/easyaws_iam_assume_role",
+			logger:        &myLogger,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := GetAssumeRoleCreds(tt.args.assumeRoleARN, tt.args.logger)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAssumeRoleCreds() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestGetAWSIdentityWithAssumeRole(t *testing.T) {
+	myLogger := log.With().Str("test_key", "test_value").Logger()
+	type args struct {
+		assumeRole string
+		logger     *zerolog.Logger
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "valid",
+			args: args{
+				assumeRole: "arn:aws:iam::709310380790:role/easyaws_iam_assume_role",
+				logger:     &myLogger,
+			}, wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAWSIdentityWithAssumeRole(tt.args.assumeRole, tt.args.logger)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAWSIdentityWithAssumeRole() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			err = ARNMatchesAssumedRole(got, "easyaws_iam_assume_role")
+			if err != nil {
+				t.Errorf("Returned ARN (%s) doesn't seem to match role name: %s", got, "easyaws_iam_assume_role")
 			}
 		})
 	}
